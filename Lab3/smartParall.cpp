7df336c53c68d1b14657c43cfdda5704fbccc1c7
daniel <den.navruzshoev@gmail.com>
2020-04-07 03:23:22 +0700

    Improving 3.3: add comments, putting out code in a function

--- Lab3/smartParall.cpp
+++ Lab3/smartParall.cpp
@@ -13,19 +13,29 @@ void createComms(MPI_Comm comm2D, MPI_Comm *columns, MPI_Comm *rows);
 void fillDataForEachProc(int *dims, int *coords, int *sizeRows, int *sizeCols);
 void fillScatterAData(int *dims, int **sendCountsA, int **displsA, int sizeRowStrip, int sizeRowStripMod);
 void fillScatterBData(int *dims, int **sendCountsB, int **displsB);
+void sendRecvMatrix(double *c, double *CPart,int *dims,int *recvCountsY,int splitX,int splitY,int countA, int rank);
+void collectCMatrix(int *dims, int *coords, double *c,
+                    double *cPart, int sizeRowStrip, int sizeRowStripMod, int sizeColumnStrip,
+                    int sizeColumnStripMod,int rank);
 
 int main(int argc, char *argv[]) {
   int size, rank;
-  MPI_Init(&argc, &argv);//инициализация mpi
-  MPI_Comm_size(MPI_COMM_WORLD, &size);//получение числа процессов
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);//получение номера процесса
+  MPI_Init(&argc, &argv);
+  MPI_Comm_size(MPI_COMM_WORLD, &size);
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
   int dims[2] = {0, 0};
   int periods[2] = {0, 0};
   int reorder = 0;
   MPI_Comm comm2D;
   MPI_Dims_create(size, MAX_DIMS, dims);
   MPI_Cart_create(MPI_COMM_WORLD, MAX_DIMS, dims, periods, reorder, &comm2D);
-
+  //dims[0]=x
+  //dims[1]=y
+  /* |--------->y
+     |
+     |
+     x
+  */
   double *A;
   double *B;
   double *C;
@@ -109,10 +119,6 @@ void caluclate(double *a, double *b, double *c, int *dims, int rank, MPI_Comm co
 
   if (coords[1] == 0) {
     MPI_Scatterv(a, sendCountsA, displsA, MPI_DOUBLE, aPart, sizeRowsForEachProc * N, MPI_DOUBLE, 0, comm1DColumns);
-    /*printf("rank=%d recv=%d\n", rank, sizeRowsForEachProc*N);
-    for (int i = 0; i < sizeRowsForEachProc*N; ++i) {
-      printf("%f ", aPart[i]);
-    }*/
   }
 
   if (coords[0] == 0) {
@@ -124,171 +130,153 @@ void caluclate(double *a, double *b, double *c, int *dims, int rank, MPI_Comm co
     }
     if (dims[1] - 1 == coords[1])
       MPI_Recv(bPart, sizeColumnStrip * N + sizeColumnStripMod * N, MPI_DOUBLE, 0, 0, comm1DRows, MPI_STATUS_IGNORE);
-
-    /* printf("rank=%d ", rank);
-
-     for (int i = 0; i < sizeColsForEachProc*N; ++i) {
-       printf("%f ", bPart[i]);
-     }
-     printf("\n");*/
   }
 
   MPI_Bcast(aPart, sizeRowsForEachProc * N, MPI_DOUBLE, 0, comm1DRows);
-  /*if(coords[1]==1 && coords[0]==1)
-  for (int i = 0; i < sizeRowsForEachProc*N; ++i) {
-    printf("%f ", aPart[i]);
-  }
-  printf("\n");*/
+
   MPI_Bcast(bPart, sizeColsForEachProc * N, MPI_DOUBLE, 0, comm1DColumns);
-  /* if(coords[1]==1 && coords[0]==1)
-     for (int i = 0; i < sizeColsForEachProc*N; ++i) {
-       printf("%f ", bPart[i]);
-     }*/
 
   for (int i = 0; i < sizeRowsForEachProc; ++i) {
     for (int j = 0; j < sizeColsForEachProc; ++j) {
       for (int k = 0; k < N; ++k) {
         cPart[i * sizeColsForEachProc + j] += aPart[i * N + k] * bPart[k * sizeColsForEachProc + j];
-        /*printf("c[%d]=%f\t  a[%d]=%f\t b[%d]=%f\n",i * sizeColsForEachProc + j,cPart[i * sizeColsForEachProc + j],
-            i * N + k,aPart[i * N + k],
-        k * sizeRowsForEachProc + j,bPart[k * sizeColsForEachProc + j] );*/
       }
     }
   }
 
-  int yTmp,xTmp;
-  int recvAndCountsY[dims[0]*dims[1]];
-  int recvAndCountsRowsY[dims[0]*dims[1]];
-  int recvAndCountsX[dims[0]*dims[1]];
-  int recvAndCountsRowsX[dims[0]*dims[1]];
-  for(int i = 0; i < dims[0]; i++) {
-    for(int j = 0; j < dims[1]; j++) {
-      xTmp = (i == dims[0] - 1) ? (sizeRowStrip+sizeRowStripMod) : sizeRowStrip;
-      yTmp = (j == dims[1] - 1) ? (sizeColumnStrip+sizeColumnStripMod) : sizeColumnStrip;
-      recvAndCountsY[i * dims[1] + j] = yTmp * N;
-      recvAndCountsX[i * dims[1] + j] = xTmp * N;
-      recvAndCountsRowsX[i * dims[1] + j] = xTmp;
-      recvAndCountsRowsY[i * dims[1] + j] = yTmp;
-      printf("%d %d %d %d\n",yTmp * N,xTmp * N,yTmp,xTmp );
+  collectCMatrix(dims, coords, c, cPart, sizeRowStrip, sizeRowStripMod, sizeColumnStrip, sizeColumnStripMod,rank);
+
+
+  if (rank == 0) {
+    free(sendCountsB);
+    free(displsB);
+    free(sendCountsA);
+    free(displsA);
+    MPI_Type_free(&typeB);
+  }
+  free(aPart);
+  free(bPart);
+  free(cPart);
+}
+
+void createComms(MPI_Comm comm2D, MPI_Comm *columns, MPI_Comm *rows) {
+  int remainsRow[2] = {0, 1};
+  int remainsColumns[2] = {1, 0};
+
+  MPI_Cart_sub(comm2D, remainsColumns, columns);
+  MPI_Cart_sub(comm2D, remainsRow, rows);
+}
+
+void createsTypes(MPI_Datatype *typeB, MPI_Datatype *typeBMod, int sizeColumnStrip, int sizeColumnStripMod) {
+
+  MPI_Type_vector(N, sizeColumnStrip, K, MPI_DOUBLE, typeB);
+  MPI_Type_vector(N, sizeColumnStrip + sizeColumnStripMod, K, MPI_DOUBLE, typeBMod);
+
+  MPI_Type_create_resized(*typeB, 0, sizeColumnStrip * sizeof(double), typeB);
+
+  MPI_Type_commit(typeB);
+  MPI_Type_commit(typeBMod);
+
+}
+
+void fillDataForEachProc(int *dims, int *coords, int *sizeRows, int *sizeCols) {
+  //FillSizeRowsForEachProc
+  if (coords[0] == dims[0] - 1) {
+    *sizeRows = M / dims[0] + M % dims[0];
+  } else {
+    *sizeRows = M / dims[0];
+  }
+  //FillSizeColsForEachProc
+  if (coords[1] == dims[1] - 1) {
+    *sizeCols = K / dims[1] + K % dims[1];
+  } else {
+    *sizeCols = K / dims[1];
+  }
+}
+
+void fillScatterAData(int *dims, int **sendCountsA, int **displsA, int sizeRowStrip, int sizeRowStripMod) {
+
+  *sendCountsA = (int *) calloc(dims[0], sizeof(int));
+  *displsA = (int *) calloc(dims[0], sizeof(int));
+  for (int i = 0; i < dims[0]; ++i) {
+    if (dims[0] - 1 == i) {
+      (*displsA)[i] = i * sizeRowStrip * N;
+      (*sendCountsA)[i] = (sizeRowStrip + sizeRowStripMod) * N;
+    } else {
+      (*displsA)[i] = i * sizeRowStrip * N;
+      (*sendCountsA)[i] = sizeRowStrip * N;
     }
   }
-  int count_a = recvAndCountsY[coords[1] * dims[1] + coords[1]];
-  int lines_x = recvAndCountsRowsX[
-      coords[0]* dims[1] + coords[1]];
-  int lines_y = recvAndCountsRowsY[coords[0] * dims[1] + coords[1]];
+}
+
+void fillScatterBData(int *dims, int **sendCountsB, int **displsB) {
+  *sendCountsB = (int *) calloc(dims[1], sizeof(int));
+  *displsB = (int *) calloc(dims[1], sizeof(int));
+
+  for (int i = 0; i < dims[1]; ++i) {
+    (*displsB)[i] = i;
+    (*sendCountsB)[i] = 1;
+  }
+}
+
+void collectCMatrix(int *dims, int *coords, double *c, double *cPart, int sizeRowStrip, int sizeRowStripMod,
+                    int sizeColumnStrip, int sizeColumnStripMod,int rank) {
+  int yTmp, xTmp;
+  int recvCountsY[dims[0] * dims[1]];
+  int recvCountsSplitY[dims[0] * dims[1]];
+  int recvCountsX[dims[0] * dims[1]];
+  int recvCountsSplitX[dims[0] * dims[1]];
+  for (int i = 0; i < dims[0]; i++) {
+    for (int j = 0; j < dims[1]; j++) {
+      xTmp = (i == dims[0] - 1) ? (sizeRowStrip + sizeRowStripMod) : sizeRowStrip;
+      yTmp = (j == dims[1] - 1) ? (sizeColumnStrip + sizeColumnStripMod) : sizeColumnStrip;
+      recvCountsY[i * dims[1] + j] = yTmp * N;
+      recvCountsX[i * dims[1] + j] = xTmp * N;
+      recvCountsSplitX[i * dims[1] + j] = xTmp;
+      recvCountsSplitY[i * dims[1] + j] = yTmp;
+    }
+  }
+  int countA = recvCountsY[coords[1] * dims[1] + coords[1]];
+  int splitX = recvCountsSplitX[coords[0] * dims[1] + coords[1]];
+  int splitY = recvCountsSplitY[coords[0] * dims[1] + coords[1]];
+  sendRecvMatrix(c,cPart,dims,recvCountsY,splitX,splitY,countA,rank);
+}
+
+void sendRecvMatrix(double *c, double *cPart, int *dims, int *recvCountsY, int splitX,
+    int splitY, int countA,int rank) {
   if (rank == 0) {
     int index = 0;
     for (int i = 0; i < M * K; i++) {
 
-      int trank_y = (i % K) / (K / dims[1]);
-      int trank_x = ((i - (i % K)) / M) / (M / dims[0]);
+      int trankY = (i % K) / (K / dims[1]);
+      //Zero rank gets Y coord for recv and collect (factorization+count the coord)
+      int trankX = ((i - (i % K)) / M) / (M / dims[0]);
+      //Zero rank gets X coord for recv and collect (factorization+count the coord)
 
-      if (trank_x >= dims[0]) trank_x = dims[0] - 1;
-      if (trank_y >= dims[1]) trank_y = dims[1] - 1;
+      if (trankX >= dims[0]) trankX = dims[0] - 1;//if the number goes beyond the dims => sub 1
+      if (trankY >= dims[1]) trankY = dims[1] - 1;//if the number goes beyond the dims => sub 1
 
-      int trank = trank_x * dims[1] + trank_y;
+      int trank = trankX * dims[1] + trankY;
 
       if (trank != 0) {
-         printf("recv[%d] %d from %d ", i, recvAndCountsY[trank_x * dims[0] + trank_y] / N , trank_x * dims[0] + trank_y);
+
         MPI_Recv(&c[i],
-                 recvAndCountsY[trank_x * dims[1] + trank_y] / N,
+                 recvCountsY[trankX * dims[1] + trankY] / N,
                  MPI_DOUBLE,
-                 trank_x * dims[1] + trank_y,
+                 trankX * dims[1] + trankY,
                  0,
                  MPI_COMM_WORLD,
                  MPI_STATUS_IGNORE);
-        i += (recvAndCountsY[trank_x * dims[1] + trank_y] / N) - 1;
-        printf("succsessful\n");
+        i += (recvCountsY[trankX * dims[1] + trankY] / N) - 1;
+
       } else {
         c[i] = cPart[index++];
       }
-    }}  else {
-    printf("rank = %d, lines_x = %d, lines_y = %d, count_a = %d\n", rank, lines_x, lines_y, count_a);
-    for(int i = 0; i < lines_x * lines_y; i += count_a / N) {
-      MPI_Send(&cPart[i], lines_y, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);
+    }
+  } else {
+    for (int i = 0; i < splitX * splitY; i += countA / N) {
+      MPI_Send(&cPart[i], splitY, MPI_DOUBLE, 0, 0, MPI_COMM_WORLD);//Send a line from subprocess
     }
   }
 
-    /*for (int i = 0; i < sizeRowsForEachProc; ++i) {
-      for (int j = 0; j < sizeColsForEachProc; ++j) {
-        printf("%f ",cPart[i * sizeColsForEachProc + j]);
-      }
-      printf("\n");
-    }
-    printf("\n");
-    printf("\n");*/
-
-
-    if (rank == 0) {
-      free(sendCountsB);
-      free(displsB);
-      free(sendCountsA);
-      free(displsA);
-      MPI_Type_free(&typeB);
-    }
-    free(aPart);
-    free(bPart);
-    free(cPart);
-  }
-
-  void createComms(MPI_Comm comm2D, MPI_Comm *columns, MPI_Comm *rows) {
-    int remainsRow[2] = {0, 1};
-    int remainsColumns[2] = {1, 0};
-
-    MPI_Cart_sub(comm2D, remainsColumns, columns);
-    MPI_Cart_sub(comm2D, remainsRow, rows);
-  }
-
-  void createsTypes(MPI_Datatype *typeB, MPI_Datatype *typeBMod, int sizeColumnStrip, int sizeColumnStripMod) {
-
-    MPI_Type_vector(N, sizeColumnStrip, K, MPI_DOUBLE, typeB);
-    MPI_Type_vector(N, sizeColumnStrip + sizeColumnStripMod, K, MPI_DOUBLE, typeBMod);
-
-    MPI_Type_create_resized(*typeB, 0, sizeColumnStrip * sizeof(double), typeB);
-
-    MPI_Type_commit(typeB); //регистрируем новый производный тип
-    MPI_Type_commit(typeBMod); //регистрируем новый производный тип
-
-  }
-
-  void fillDataForEachProc(int *dims, int *coords, int *sizeRows, int *sizeCols) {
-    //FillSizeRowsForEachProc
-    if (coords[0] == dims[0] - 1) {
-      *sizeRows = M / dims[0] + M % dims[0];
-    } else {
-      *sizeRows = M / dims[0];
-    }
-    //FillSizeColsForEachProc
-    if (coords[1] == dims[1] - 1) {
-      *sizeCols = K / dims[1] + K % dims[1];
-    } else {
-      *sizeCols = K / dims[1];
-    }
-  }
-
-  void fillScatterAData(int *dims, int **sendCountsA, int **displsA, int sizeRowStrip, int sizeRowStripMod) {
-
-    *sendCountsA = (int *) calloc(dims[0], sizeof(int));
-    *displsA = (int *) calloc(dims[0], sizeof(int));
-    for (int i = 0; i < dims[0]; ++i) {
-      if (dims[0] - 1 == i) {
-        (*displsA)[i] = i * sizeRowStrip * N;
-        (*sendCountsA)[i] = (sizeRowStrip + sizeRowStripMod) * N;
-      } else {
-        (*displsA)[i] = i * sizeRowStrip * N;
-        (*sendCountsA)[i] = sizeRowStrip * N;
-      }
-    }
-  }
-
-  void fillScatterBData(int *dims, int **sendCountsB, int **displsB) {
-    *sendCountsB = (int *) calloc(dims[1], sizeof(int));
-    *displsB = (int *) calloc(dims[1], sizeof(int));
-
-    for (int i = 0; i < dims[1]; ++i) {
-      (*displsB)[i] = i;
-      (*sendCountsB)[i] = 1;
-    }
-  }
-
-
+}
